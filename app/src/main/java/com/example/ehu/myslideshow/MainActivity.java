49a0c7c7d850e9cd3947293fa0e0594b681935fb
ehu <fehuehuf@gmail.com>
2017-12-30 21:47:55 +0900

    ページ送りのアニメーション実装

--- app/src/main/java/com/example/ehu/myslideshow/MainActivity.java
+++ app/src/main/java/com/example/ehu/myslideshow/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.ehu.myslideshow;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageSwitcher mImageSwitcher;
+    int[] mImageresources = {R.drawable.slide00, R.drawable.slide01,
+            R.drawable.slide02, R.drawable.slide03,
+            R.drawable.slide04, R.drawable.slide05,
+            R.drawable.slide06, R.drawable.slide07,
+            R.drawable.slide08, R.drawable.slide09};
+
+    int mPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        mImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                return imageView;
+            }
+        });
+        mImageSwitcher.setImageResource(mImageresources[0]);
+    }
+
+    public void onAnimationButtonTapped(final View view) {
+        float y = view.getY() + 100;
+        view.animate().setDuration(1000).setInterpolator(new BounceInterpolator()).y(y);
+    }
+
+    private void movePosition(int move) {
+        mPosition = mPosition + move;
+        if (mPosition >= mImageresources.length) {
+            mPosition = 0;
+        } else if (mPosition < 0) {
+            mPosition = mImageresources.length - 1;
+        }
+        mImageSwitcher.setImageResource(mImageresources[mPosition]);
+    }
+    @SuppressLint("WrongViewCast")
+    public void onPrevButtonTapped(View view) {
+        mImageSwitcher.setInAnimation(this, android.R.anim.fade_in);
+        mImageSwitcher.setOutAnimation(this, android.R.anim.fade_out);
+        movePosition(-1);
+        findViewById(R.id.imageView).animate().setDuration(1000).alpha(0.0f);
+    }
+
+    @SuppressLint("WrongViewCast")
+    public void onNextButtonTapped(View view) {
+        mImageSwitcher.setInAnimation(this, android.R.anim.slide_in_left);
+        mImageSwitcher.setOutAnimation(this, android.R.anim.slide_out_right);
+        movePosition(1);
+        findViewById(R.id.imageView).animate().setDuration(1000).alpha(0.0f);
     }
 }
